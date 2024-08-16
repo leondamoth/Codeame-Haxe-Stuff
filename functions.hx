eb626b189637d6169e687b6d865219cea7bd5e37
leondamoth <leondamoth@gmail.com>
2024-08-16 16:26:17 -0400

    Update functions.hx

--- functions.hx
+++ functions.hx
@@ -1,7 +1,15 @@
-function postCreate(){ // Runs only ONCE when the playstate loads
+function create(){ // similar to onCreate in lua, where it creates everytime the playstate is loaded(runs ONCE)
 
 }
 
-function postUpdate(elapsed){ // runs for that whole playstate, constantly updating everytime
+function postCreate(){ // runs only ONCE when the create function is finished
+
+}
+
+function updating(){ // happens while the playstate is constatly updating(rus throught the WhOLE playstate)
+
+}
+
+function postUpdate(elapsed){ // runs for that whole playstate, constantly updating everytime after the updating is finished
 
 }
