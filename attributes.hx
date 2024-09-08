a6356ed36b0d228e7fba7b4d8e38c9df0860432b
leondamoth <leondamoth@gmail.com>
2024-09-08 13:49:35 -0400

    Update attributes.hx

--- attributes.hx
+++ attributes.hx
@@ -1,2 +1,7 @@
 // static: used for global variables, used when the variable or method is not a child (the shit in the brackets) of the class.
 
+
+//extends: whatever class is before this keyword is the child of the class that's after this keyword.
+//ex. public class Funkin extends FridayNight()
+// funkin is the child class and FridayNight is the parent class(superclass)
+
