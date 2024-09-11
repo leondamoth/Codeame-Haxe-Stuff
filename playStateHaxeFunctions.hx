324629a9bb7d015015602251bb9f185ce5e7e2a0
leondamoth <leondamoth@gmail.com>
2024-09-11 18:05:10 -0400

    Update playStateHaxeFunctions.hx

--- playStateHaxeFunctions.hx
+++ playStateHaxeFunctions.hx
@@ -1,1 +1,68 @@
+ function onRatingUpdate(RatingUpdateEvent)
+
+function create()
+
+function onPreGenerateStrums(AmountEvent)
+
+function onPostGenerateStrums(AmountEvent)
+
+function postCreate()
+
+function onStartCountdown(CancellableEvent)
+
+function onPostStartCountdown()
+
+function onCountdown(CountdownEvent)
+
+function onSongStart()
+
+function onStartSong()
+
+function destroy()
+
+function onSubstateOpen(StateEvent)
+
+function onSubstateClose(StateEvent)
+
+function onFocus()
+
+function onFocusLost()
+
+function onVocalsResync()
+
+function onGamePause(CancellableEvent)
+
+function update(elapsed:Float)
+
+function onCameraMove(CamMoveEvent)
+
+function postUpdate(elapsed:Float)
+
+function draw(DrawEvent)
+
+function postDraw(DrawEvent)
+
+function onEvent(EventGameEvent)
+
+function onGameOver(GameOverEvent)
+
+function onPostGameOver(GameOverEvent)
+
+function onSongEnd()
+
+function onPlayerMiss(NoteMissEvent)
+
+function onPlayerHit(NoteHitEvent)
+
+function onDadHit(NoteHitEvent)
+
+function onNoteHit(NoteHitEvent)
+
+function stepHit(curStep:Int)
+
+function beatHit(curBeat:Int)
+
+function measureHit(curMeasure:Int)
+
+
 
